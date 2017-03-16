c858a8ba16a4a22768984516e27cd6014d19c854
Yangmu Jiang <celsius.j@gmail.com>
2017-03-15 20:44:32 -0400

    remove dep code

--- demo/deviceQuery.cpp
+++ demo/deviceQuery.cpp
@@ -17,7 +17,67 @@
 #include <iostream>
 
 #include <cuda_runtime.h>
-#include <helper_cuda.h>
+//#include <helper_cuda.h>
+// Beginning of GPU Architecture definitions
+inline int _ConvertSMVer2Cores(int major, int minor)
+{
+	// Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+	typedef struct
+	{
+		int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+		int Cores;
+	} sSMtoCores;
+
+	sSMtoCores nGpuArchCoresPerSM[] =
+	{
+		{ 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+		{ 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+		{ 0x30, 192 }, // Kepler Generation (SM 3.0) GK10x class
+		{ 0x32, 192 }, // Kepler Generation (SM 3.2) GK10x class
+		{ 0x35, 192 }, // Kepler Generation (SM 3.5) GK11x class
+		{ 0x37, 192 }, // Kepler Generation (SM 3.7) GK21x class
+		{ 0x50, 128 }, // Maxwell Generation (SM 5.0) GM10x class
+		{ 0x52, 128 }, // Maxwell Generation (SM 5.2) GM20x class
+		{ 0x53, 128 }, // Maxwell Generation (SM 5.3) GM20x class
+		{ 0x60, 64 }, // Pascal Generation (SM 6.0) GP100 class
+		{ 0x61, 128 }, // Pascal Generation (SM 6.1) GP10x class
+		{ 0x62, 128 }, // Pascal Generation (SM 6.2) GP10x class
+		{ -1, -1 }
+	};
+
+	int index = 0;
+
+	while (nGpuArchCoresPerSM[index].SM != -1)
+	{
+		if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor))
+		{
+			return nGpuArchCoresPerSM[index].Cores;
+		}
+
+		index++;
+	}
+
+	// If we don't find the values, we default use the previous one to run properly
+	printf("MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n", major, minor, nGpuArchCoresPerSM[index - 1].Cores);
+	return nGpuArchCoresPerSM[index - 1].Cores;
+}
+#ifndef SPRINTF
+#define SPRINTF sprintf_s
+#endif
+template< typename T >
+void check(T result, char const *const func, const char *const file, int const line)
+{
+	if (result)
+	{
+		/*fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n",
+				file, line, static_cast<unsigned int>(result), _cudaGetErrorEnum(result), func);
+		DEVICE_RESET*/
+			// Make sure we call CUDA Device Reset before exiting
+			exit(EXIT_FAILURE);
+	}
+}
+// This will output the proper CUDA error strings in the event that a CUDA host call returns an error
+#define checkCudaErrors(val)           check ( (val), #val, __FILE__, __LINE__ )
 
 
 
