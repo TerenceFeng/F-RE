93f6a72e7073bf22f4186452141eec67b543e598
Terence Feng <tyxfeng@gmail.com>
2017-08-05 17:24:18 -0400

    merge World removed ply corresponding makefile change

--- cpu/World.h
+++ cpu/World.h
@@ -30,11 +30,29 @@ public:
 	std::vector<Light *> light_ptrs;
 	AmbientOccluder *ambient_ptr;
 
-	World(void);
-	~World(void);
+    World(void):
+        background_color(BLACK)
+    {}
 
-	void add_object(Object *obj);
-	void add_light(Light *light_ptr);
+    ~World(void)
+    {
+        for (int i = 0; i < obj_ptrs.size(); i++) {
+            delete obj_ptrs[i];
+        }
+        for (int i = 0; i < light_ptrs.size(); i++) {
+            delete light_ptrs[i];
+        }
+    }
+
+    void add_object(Object *obj_ptr)
+    {
+        obj_ptrs.push_back(obj_ptr);
+    }
+    void add_light(Light *light_ptr)
+    {
+        light_ptrs.push_back(light_ptr);
+    }
+
 
 };
 
